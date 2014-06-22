5d10f32f34f1446ce1b4f2b5af0868c7895d76c9
Christiaan Druif <cm.druif@gmail.com>
2014-06-22 17:23:15 +0200

    Multiple actions can be used in a rule

--- src/core/test/Trigger.py
+++ src/core/test/Trigger.py
@@ -15,14 +15,19 @@ class Trigger():
                 if match:            
                     rule_action = rule.get(x)
             
-            action_target = folder + rule_action
-            action_target = action_target.replace(" ", "").replace("'", '')
+            actions = rule_action.split(',')
+            for action in actions:
+                action = action.replace("'","" ).replace(" ", '')
+                
+                action_target = folder + action
+                action_target = action_target.replace(" ", "").replace("'", '')
+                
                              
-            if os.path.exists(action_target) is True:
-                print 'The action rule is : ', action_target
-                subprocess.call([sys.executable, action_target, rule])
-            else:
-                print 'The action rule is not valid. Please use the correct path of the file.'
+                if os.path.exists(action_target) is True:
+                    print 'The action rule is : ', action_target
+                    subprocess.call([sys.executable, action_target, rule['DESCRIPTION =']])
+                else:
+                    print 'The action rule is not valid. Please use the correct path of the file.'
             
         else:
             # There is no action needed.
